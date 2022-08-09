caf6b3dff5d09d506ef474ccf72c1b2126589ecd
sykrn <syukron@outlook.com>
2022-08-09 13:10:41 +0700

    add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from fastapi import APIRouter, FastAPI, Request
+from fastapi import File, UploadFile, FastAPI
 from fastapi.responses import HTMLResponse,FileResponse
 import secureqr
 from lxml import etree as ET
@@ -14,7 +15,29 @@ templates = Jinja2Templates(directory="templates")
 async def generateSQR(v:int,box:int,data:str):       
     return ET.tostring(secureqr.generateSVGSQR(data,v,box))
 
-@app.get("/verify_sqr")
+@app.post("/verify_sqr")
 async def verify(v:int,data:str,seq:str,dot:str):  
     print("here...")     
     return  {"match_percent": secureqr.percentage_matching(data,seq,dot,version=v)}
+
+
+# @app.post("/verify_isqr")
+# def upload(file: UploadFile = File(...)):
+#     try:
+#         contents = file.file.read()
+#         with open(file.filename, 'wb') as f:
+#             f.write(contents)
+#     except Exception:
+#         return {"message": "There was an error uploading the file"}
+#     finally:
+#         file.file.close()
+
+#     return {"message": f"Successfully uploaded {file.filename}"}
+
+
+# from PIL import Image
+# import io
+
+# image_data = ... # byte values of the image
+# image = Image.open(io.BytesIO(image_data))
+# image.show()
