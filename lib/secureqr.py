7a00e3814a84c0d0cf13c4a58a02df7d77cf5f46
syukron alfarozi <40594982+sykrn@users.noreply.github.com>
2022-09-28 06:47:01 +0700

    add upload image handler

--- lib/secureqr.py
+++ lib/secureqr.py
@@ -11,6 +11,82 @@ from lxml import etree as ET
 SECRET = 'rispro'
 # SALT = ''.join(format(ord(x), 'x') for x in SECRET)
 
+def generate_watermark(data,secret,quant=16,size=(100,100),rprop=100):
+    dist  = [0 if i==0 else int(i*256/quant)-1 for i in range(quant+1)]
+    np.random.seed(abs(hash(data+secret))%2**32)
+    p=np.random.choice([i for i in range(rprop)], size = quant+1)
+    p=p/p.sum()
+    watermark = np.random.choice(dist,size=size,p=p)
+    return watermark.astype('uint8')
+
+def make_secureQR(data:str,secret:str=SECRET,                  
+                  pct_wm_size:int=0.3, 
+                  bx_size:int=10,
+                  border:int=4,
+                  qr_ver:int=None,
+                  **kwargs)-> np.ndarray:
+
+    img = qrcode.make(data,version=qr_ver, box_size=bx_size, border=border, error_correction=qrcode.constants.ERROR_CORRECT_H)
+    img = np.asanyarray(img).astype('uint8')*255
+    
+    wm_size = (img.shape[0]//bx_size - 2*border) * pct_wm_size
+    wm_size = int(wm_size)
+    if not wm_size%2==0: wm_size-=1 
+
+    wm_size *= bx_size
+    size = (wm_size,wm_size)
+    watermark = generate_watermark(data,secret,size=size,**kwargs)
+    wm = np.pad(watermark, bx_size//2, constant_values=255)
+
+    # add border watermark 8
+    length = wm.shape[0] 
+    loc = img.shape[0]//2 - length//2
+    img[loc:loc+length,loc:loc+length]= wm
+    return img, watermark
+
+import cv2
+import numpy as np
+from scipy.special import rel_entr
+from scipy.spatial import distance
+
+def read_sqr(im):
+    qrDecoder = cv2.QRCodeDetector()
+    myqr = qrDecoder.detectAndDecode(im)
+    top = myqr[1][0][0].astype(int)
+    bottom = myqr[1][0][2].astype(int)+1
+    qim = im[top[0]:bottom[0],top[1]:bottom[1]].astype(float)
+
+    # # normalize
+    qim -= qim.min()
+    qim *= 255/qim.max()
+
+    return myqr[0],qim
+
+def JS_div(p,q):
+    m = (p+q)/2
+    div = 0.5*rel_entr(p,m) + 0.5*rel_entr(q,m)
+    return div.sum()
+
+def compute_KL(qim,scan,quant=16,pct=0.2,metric='JS'):
+    pq = []
+    for q in [qim,scan]:
+        l = q.shape[0]
+        half = l//2
+        r = int(0.5*pct*l)             
+        wtm = q[half-r:half+r,half-r:half+r]
+        dist, *note = np.histogram(wtm.ravel(),bins=quant)
+        p = dist/dist.sum()
+        pq.append(p)        
+    return rel_entr(*pq).sum() if metric=="KL" else distance.jensenshannon(*pq), pq  
+
+def compare2template(im, qr_ver = 3, quant=16, pct=0.2, metric='JS'):
+    data ,qim = read_sqr(im) 
+    img,_ = make_secureQR(data, qr_ver = qr_ver, quant=quant)
+    _,qim_scan = read_sqr(img) 
+    return (data,)+compute_KL(qim,qim_scan,quant,pct,metric)
+
+
+# OLD code
 def get_data_ids(n=29):
     ids = np.array([[[j,i] for i in range(n)] for j in range(n)])
     # mask corner
--- main.py
+++ main.py
@@ -3,41 +3,59 @@ import json
 import uvicorn
 from fastapi import APIRouter, FastAPI, Request
 from fastapi import File, UploadFile, FastAPI
-from fastapi.responses import HTMLResponse,FileResponse
+from fastapi.responses import HTMLResponse, Response
 import lib.secureqr as secureqr
 from lxml import etree as ET
 from fastapi.templating import Jinja2Templates
+import numpy as np
+from PIL import Image
+import io
+from imageio import v3 as iio
+
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/generate_sqr", response_class=HTMLResponse)
-async def generateSQR(v:int,box:int,data:str):       
-    return ET.tostring(secureqr.generateSVGSQR(data,v,box))
+# @app.get("/generate_sqr", response_class=HTMLResponse)
+# async def generateSQR(v:int,box:int,data:str):       
+#     return ET.tostring(secureqr.generateSVGSQR(data,v,box))
 
-@app.post("/verify_sqr")
-async def verify(v:int,data:str,seq:str,dot:str):  
-    print("here...")     
-    return  {"match_percent": secureqr.percentage_matching(data,seq,dot,version=v)}
+@app.get("/watermark_sqr", response_class=HTMLResponse)
+async def watermarkSQR(v:int,quant:int,data:str):   
+    im,wm = secureqr.make_secureQR(data,qr_ver=v,quant=quant)
+    img = Image.fromarray(im)
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+        
+    headers = {'Content-Disposition': 'inline; filename="SQR.png"'}
+    return Response(img_byte_arr, headers=headers, media_type='image/png')
+
+
+@app.post("/verify_wsqr")
+def upload(v:int,quant:int,pct:float, file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        im = np.asanyarray(Image.open(io.BytesIO(contents)))
+        data,s,(p,q) = secureqr.compare2template(im,v,quant,pct)        
+     
+        # with open(file.filename, 'wb') as f:
+        #     f.write(contents)
+    except Exception as e:
+        return {"message": "There was an error uploading the file","error":str(e)}
+    finally:
+        file.file.close()
+    p = p.tolist()
+    q = q.tolist()
+    return {"data":data,"KL-div-score": s,"actual-dist": p, "scanned-dist": q}
+    
+
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000,debug=True)
+    uvicorn.run("main:app", host="localhost", port=8000, debug=True,reload=True)
 
-# @app.post("/verify_isqr")
-# def upload(file: UploadFile = File(...)):
-#     try:
-#         contents = file.file.read()
-#         with open(file.filename, 'wb') as f:
-#             f.write(contents)
-#     except Exception:
-#         return {"message": "There was an error uploading the file"}
-#     finally:
-#         file.file.close()
-
-#     return {"message": f"Successfully uploaded {file.filename}"}
 
 
 # from PIL import Image
